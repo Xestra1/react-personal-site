73db507b9dd01178122f17574e6443d59139d66b
Nick <nickxz149@gmail.com>
2019-11-15 19:42:00 -0500

    update: 1
    could release it but want to do more with the project before posting it.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import './App.scss';
 
 class App extends React.Component {
@@ -16,24 +17,46 @@ class App extends React.Component {
 
     // STYLING
     const background = {
-      background: "black", // ADD URL HERE
+      background: "", // ADD URL HERE
     }
     const text = {
-      color: "white", // ADD COLOR HERE
+      color: "", // ADD COLOR HERE
     }
     const hr = {
-      background: "white", // ADD COLOR HERE
-      borderColor: "white", // ADD COLOR HERE
+      background: "", // ADD COLOR HERE
+      borderColor: "", // ADD COLOR HERE
     }
 
     // RETURNS HTML
     return (
-      <div className="body" style={background}>
-        <div className="title">
-          <p style={text}>{this.state.title}</p>
-          <hr style={hr} />
-          <h1 style={text}>{this.state.name}</h1>
-          <hr style={hr} />
+      <div>
+        <Particles
+          className="particles"
+          params={{
+            "particles": {
+              "number": {
+                "value": 50
+              },
+              "size": {
+                "value": 1
+              }
+            },
+            "interactivity": {
+              "events": {
+                "onhover": {
+                  "enable": false,
+                  "mode": "repulse"
+                }
+              }
+            }
+          }} />
+        <div className="body" style={background}>
+          <div className="title">
+            <p style={text}>{this.state.title}</p>
+            <hr className="left" style={hr} />
+            <h1 style={text}>{this.state.name}</h1>
+            <hr className="right" style={hr} />
+          </div>
         </div>
       </div>
     );
